0830976ae6d834ad3af6c9dba3f7539bc4b867f5
kuu13580 <13580kuu@gmail.com>
2023-04-26 17:48:36 +0900

    開発者にALL権限付与

--- db_setup.sql
+++ db_setup.sql
@@ -1,4 +1,4 @@
 -- mysql -u root < <このファイル>
 CREATE DATABASE schedule_development CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'schedule_development'@'localhost' IDENTIFIED BY 'password';
-GRANT CREATE, SELECT, INSERT, UPDATE, DELETE ON schedule_development.* TO 'schedule_development'@'localhost';
+GRANT ALL ON schedule_development.* TO 'schedule_development'@'localhost';
