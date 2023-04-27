b603e87f1bb9ede750af8b2ae89a4aad093daa1e
kuu13580 <13580kuu@gmail.com>
2023-04-27 20:57:13 +0900

    apiルート追加

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('events', 'App\Http\Controllers\EventController@index');
+Route::get('users', 'App\Http\Controllers\UserController@index');
