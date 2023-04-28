f1c686f92d770d45e933f5b0317d946c2671984d
kuu13580 <13580kuu@gmail.com>
2023-04-28 22:37:36 +0900

    イベント作成APIを実装

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\EventOwnerRel;
+use App\Models\User;
 
 class EventsController extends Controller
 {
@@ -28,6 +29,43 @@ class EventsController extends Controller
         $event['owner_id'] = $owner_id;
         return response()->json(
             $event, 
+            200, 
+            [], 
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+
+    public function createEvent(Request $request){
+        $attr = $request->validate([
+            'name' => 'required|string|max:256',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+            'owner_name' => 'required|string|max:256',
+        ]);
+
+        // Eventの作成
+        $event = new Event;
+        $event->name = $attr['name'];
+        $event->start_date = $attr['start_date'];
+        $event->end_date = $attr['end_date'];
+        $pass = substr(hash('sha256', $attr['name'].date('Y-m-d-H-i-s')), 0, 5);
+        $event->hash = hash('sha256', $pass);
+        $event->save();
+
+        // ownerユーザーの作成
+        $owner = new User;
+        $owner->event_id = $event->id;
+        $owner->name = $attr['owner_name'];
+        $owner->save();
+
+        // EventOwnerRelの作成
+        $rel = new EventOwnerRel;
+        $rel->event_id = $event->id;
+        $rel->owner_id = $owner->id;
+        $rel->save();
+
+        return response()->json(
+            ['event_id' => $event->id, 'pass' => $pass],
             200, 
             [], 
             JSON_UNESCAPED_UNICODE
