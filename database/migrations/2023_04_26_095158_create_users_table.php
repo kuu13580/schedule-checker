bfb56e9fe199cee43cc4dd488413d5299ee790da
kuu13580 <13580kuu@gmail.com>
2023-04-28 16:07:18 +0900

    users tableの初期設定

--- database/migrations/2023_04_26_095158_create_users_table.php
+++ database/migrations/2023_04_26_095158_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('event_id')->constrained('events');
+            $table->string('name');
             $table->timestamps();
         });
     }
