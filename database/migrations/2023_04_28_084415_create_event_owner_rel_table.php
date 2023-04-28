b4c43716e7158fe762aa3181f5d36d335c3d7201
kuu13580 <13580kuu@gmail.com>
2023-04-28 18:52:11 +0900

    timestamp追加

--- database/migrations/2023_04_28_084415_create_event_owner_rel_table.php
+++ database/migrations/2023_04_28_084415_create_event_owner_rel_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('event_owner_rel', function (Blueprint $table) {
             $table->string('event_id')->nullable(false);
             $table->bigInteger('owner_id')->unsigned()->nullable(false);
+            $table->timestamps();
         });
     }
 
