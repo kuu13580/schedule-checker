a9bafd98c7cc76ec93c5fbc85a2e2bbf50778cf1
kuu13580 <13580kuu@gmail.com>
2023-04-28 21:21:13 +0900

    Util関数を定義

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function isHashMatch($target, $hash){
+        $hashed_target = hash('sha256', $target);
+        if($hashed_target === $hash){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    protected $hash_error_responce = response()->json(
+        ['message' => 'Hash is not valid'],
+        403,
+        [],
+        JSON_UNESCAPED_UNICODE
+    );
 }
