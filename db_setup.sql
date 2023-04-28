39fbc94f9bcee3e766087a4d0b64de4850d521d5
kuu13580 <13580kuu@gmail.com>
2023-04-28 17:05:25 +0900

    db_setupで削除も実行できるよう修正

--- db_setup.sql
+++ db_setup.sql
@@ -1,4 +1,6 @@
 -- mysql -u root < <このファイル>
+DROP DATABASE IF EXISTS schedule_development;
 CREATE DATABASE schedule_development CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+DROP USER IF EXISTS 'schedule_development'@'localhost';
 CREATE USER 'schedule_development'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL ON schedule_development.* TO 'schedule_development'@'localhost';
