fb35d179ae4218f1c42b9815b01873dd3c941bf2
kuu13580 <13580kuu@gmail.com>
2023-04-28 16:05:59 +0900

    events tableの初期設定

--- database/migrations/2023_04_26_095041_create_events_table.php
+++ database/migrations/2023_04_26_095041_create_events_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->id();
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
