a207c0fa5a61464d09b805f65e88dd167c068b22
kuu13580 <13580kuu@gmail.com>
2023-05-01 18:32:13 +0900

    祝日表示に対応

--- fe/src/components/CalenderDay.tsx
+++ fe/src/components/CalenderDay.tsx
@@ -5,12 +5,13 @@ export const CalenderDay = (props: { date: Dayjs, status: string | null }) => {
   const date = props.date;
   const status = props.status;
   const day = date.format('D');
+  const Holidays = require('japanese-holidays');
   const bgColor =
     status === 'busy' ? 'red'
     : status === 'danger' ? 'yellow'
     : 'white';
   const dateTextColor = 
-    date.weekday() === 0 ? 'red':
+    date.weekday() === 0 || Holidays.isHoliday(date.toDate()) ? 'red':
     date.weekday() === 6 ? 'blue':
     'black';
   return (
