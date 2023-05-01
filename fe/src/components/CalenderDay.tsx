f7aba9c50663732d03aeb8281eb5337235026b08
kuu13580 <13580kuu@gmail.com>
2023-05-01 17:55:05 +0900

    Calenderをあらかた完成

--- fe/src/components/CalenderDay.tsx
+++ fe/src/components/CalenderDay.tsx
@@ -1,17 +1,28 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Paper } from '@mui/material';
 import { Dayjs } from 'dayjs';
 
-export const CalenderDay = (props : {date : Dayjs, status : string | null}) => {
+export const CalenderDay = (props: { date: Dayjs, status: string | null }) => {
   const date = props.date;
   const status = props.status;
   const day = date.format('D');
-  const bgColor = 
-    status === 'busy' ? 'red' 
+  const bgColor =
+    status === 'busy' ? 'red'
     : status === 'danger' ? 'yellow'
     : 'white';
+  const dateTextColor = 
+    date.weekday() === 0 ? 'red':
+    date.weekday() === 6 ? 'blue':
+    'black';
   return (
-    <Box sx={{width : 'calc(90svw / 7)', height : 'calc(90svw / 7)', bgcolor: bgColor}}>
-      <Typography>{ day }</Typography>
+    <Box 
+      sx={{width : '100%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',}}>
+      <Paper sx={{ bgcolor: bgColor, width : '90%', height: '90%'}}>
+        <Typography sx={{ color: dateTextColor }}>{day}</Typography>
+      </Paper>
     </Box>
   );
 }
--- fe/src/components/CalenderMonth.tsx
+++ fe/src/components/CalenderMonth.tsx
@@ -1,9 +1,32 @@
-import Dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import weekday from 'dayjs/plugin/weekday'
 import { CalenderDay } from './CalenderDay';
+import Grid from '@mui/material/Unstable_Grid2';
+import { AspectRatio } from '@mui/joy';
+import { Schedule } from '../models';
 
-export const CalenderMonth = (props : {date : Dayjs}) => {
+dayjs.extend(weekday);
 
+export const CalenderMonth = (props: { month: Dayjs, data: Schedule | null}) => {
+  const month = props.month.startOf('month');
+  const data = props.data;
+  // 表示する日付の配列を作成
+  const numDays = Math.ceil((month.daysInMonth() + month.weekday()) / 7) * 7;
+  const startDate = month.startOf('month').subtract(month.weekday(), 'day');
+  const daysArray = Array.from({length: numDays}, (_, i) => startDate.add(i, 'day'));
   return (
-    <></>
+    <Grid container spacing={0} justifyContent="space-between">
+      {
+        daysArray.map((date, index) => {
+          return (
+            <AspectRatio sx={{width : 'calc(100% / 7)'}} ratio={1}>
+              <Grid xs={1}>
+                <CalenderDay key={index} date={date} status={null}/>
+              </Grid>
+            </AspectRatio>
+          );
+        })
+      }
+    </Grid>
   );
 }
