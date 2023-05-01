030aae2e65725f929f2b679c0c0044160c15fd13
kuu13580 <13580kuu@gmail.com>
2023-05-01 16:44:26 +0900

    statusのnull許容

--- fe/src/components/CalenderDay.tsx
+++ fe/src/components/CalenderDay.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import { Dayjs } from 'dayjs';
 
-export const CalenderDay = (props : {date : Dayjs, status : string}) => {
+export const CalenderDay = (props : {date : Dayjs, status : string | null}) => {
   const date = props.date;
   const status = props.status;
   const day = date.format('D');
