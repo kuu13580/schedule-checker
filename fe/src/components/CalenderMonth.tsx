1bd1bcad87e708c45c552198b479b7698c48d066
kuu13580 <13580kuu@gmail.com>
2023-05-01 16:20:39 +0900

    add CalenderMonth

---
+++ fe/src/components/CalenderMonth.tsx
@@ -0,0 +1,9 @@
+import Dayjs from 'dayjs';
+import { CalenderDay } from './CalenderDay';
+
+export const CalenderMonth = (props : {date : Dayjs}) => {
+
+  return (
+    <></>
+  );
+}
--- fe/src/components/index.ts
+++ fe/src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './CalenderDay';
+export * from './CalenderMonth';
