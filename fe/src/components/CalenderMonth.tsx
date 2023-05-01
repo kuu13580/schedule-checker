9c882a1a7bd7768f23b7c6cdae4edb0d116bd817
kuu13580 <13580kuu@gmail.com>
2023-05-01 22:36:18 +0900

    status変更を実装

--- fe/src/components/CalenderMonth.tsx
+++ fe/src/components/CalenderMonth.tsx
@@ -4,10 +4,12 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { AspectRatio } from '@mui/joy';
 import { DateRange, Schedule } from '../models';
 
-export const CalenderMonth = (props: { month: Dayjs, data: Schedule[], range: DateRange}) => {
+export const CalenderMonth = (props: { month: Dayjs, data: Schedule[], range: DateRange, setDataById: any}) => {
   const month = props.month.startOf('month');
   const data = props.data;
   const range = props.range;
+  const setDataById = props.setDataById;
+
   // 表示する日付の配列を作成
   const numDays = Math.ceil((month.daysInMonth() + month.weekday()) / 7) * 7;
   const startDate = month.startOf('month').subtract(month.weekday(), 'day');
@@ -16,12 +18,18 @@ export const CalenderMonth = (props: { month: Dayjs, data: Schedule[], range: Da
     const target = data.find((d) => d.date.isSame(date));
     return target ? target.status : '';
   });
+
+  // 最初の日付に対応するidを取得
+  const firstDateId = 
+    daysArray[0].isBefore(range.start) ? daysArray[0].diff(range.start, 'day') :
+    data.findIndex((d) => d.date.isSame(daysArray[0]));
+
   return (
     <Grid container spacing={0} justifyContent="space-between">
       {
         daysArray.map((date, index) => {
           return (
-            <AspectRatio key={index} sx={{width : 'calc(100% / 7)'}} ratio={1}>
+            <AspectRatio onClick={() => {setDataById(firstDateId + index)}} key={index} sx={{width : 'calc(100% / 7)'}} ratio={1}>
               <Grid xs={1}>
                 <CalenderDay 
                   date={date} 
