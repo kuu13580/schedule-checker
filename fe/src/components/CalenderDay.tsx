4cdce428a3338facdfe1f953ad034c14e65dc7b6
kuu13580 <13580kuu@gmail.com>
2023-05-01 16:11:50 +0900

    仮CalenderDayを実装

---
+++ fe/src/components/CalenderDay.tsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from '@mui/material';
+import { Dayjs } from 'dayjs';
+
+export const CalenderDay = (props : {date : Dayjs, status : string}) => {
+  const date = props.date;
+  const status = props.status;
+  const day = date.format('D');
+  const bgColor = 
+    status === 'busy' ? 'red' 
+    : status === 'danger' ? 'yellow'
+    : 'white';
+  return (
+    <Box sx={{width : 'calc(90svw / 7)', height : 'calc(90svw / 7)', bgcolor: bgColor}}>
+      <Typography>{ day }</Typography>
+    </Box>
+  );
+}
