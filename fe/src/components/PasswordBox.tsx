9a32d20eb92e4ff48e8799e4fc13c9c17831d33d
kuu13580 <13580kuu@gmail.com>
2023-05-25 23:59:27 +0900

    PasswordBoxを実装

--- fe/src/components/PasswordBox.tsx
+++ fe/src/components/PasswordBox.tsx
@@ -1,6 +1,33 @@
+import { TextField } from '@mui/material';
+import { useState } from 'react';
+
 export const PasswordBox = () => {
+
+  const [password, setPassword] = useState<string>('');
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    const lastDigit = inputValue.slice(-1);
+    
+    setPassword(inputValue);
+    if (inputValue.length === 4) authenticate(inputValue);
+  };
+
+  const authenticate = (password: string) => {
+    // パスワード認証
+    console.log("authenticate!");
+  }
+
   return (
     <>
+      <TextField
+        onChange={handlePasswordChange}
+        value={password}
+        label="Password"
+        type="number"
+        margin='normal'
+        inputProps={{ maxLength: 4 }}
+        />
     </>
   )
 }
