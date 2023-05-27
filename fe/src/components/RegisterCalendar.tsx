e2d47ee6270cc1484e4281b4705d3c0a31b6bbd5
kuu13580 <13580kuu@gmail.com>
2023-05-28 01:58:41 +0900

    RegisterCalenderにモジュール化

--- fe/src/components/RegisterCalendar.tsx
+++ fe/src/components/RegisterCalendar.tsx
@@ -1,23 +1,131 @@
-import { Calendar, ConfigProvider } from "antd";
-import { Schedule } from "../models";
-import { DateRange } from "../models";
+import { Calendar, ConfigProvider, message } from "antd";
+import { Schedule, DateRange } from "../models";
 import { Dayjs } from "dayjs";
 import jaJP from 'antd/lib/locale/ja_JP';
 import "../styles/RegisterCalendar.css"
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { LoadingBackdrop, StatusRadio } from "./";
 
-export const RegisterCalendar = (props: { data: Schedule[], range: DateRange, setStatusByDate: (date: Dayjs) => void}) => {
-  const data = props.data;
-  const range = props.range;
-  const setStatusByDate = props.setStatusByDate;
+// 仮データ
+const userId = "1";
 
+export const RegisterCalendar = (props: {password: string}) => {
+  const password = props.password;
+
+  // パスパラメータ取得
+  const { eventId, hash } = useParams<{eventId: string, hash: string}>();
+
+  const [eventName, setEventName] = useState<string>('');
   const [selectedValue, setSelectedValue] = useState<Dayjs>(dayjs());
+  const [data, setData] = useState<Schedule[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<string>('unavailable');
+  const [dateRange, setDateRange] = useState<DateRange>({start: dayjs(), end: dayjs()});
+  const [showLoading, setShowLoading] = useState<boolean>(false);
+  const [messageApi, contextHolder] = message.useMessage();
+
+    // メッセージ表示
+    const error = (msg: string) => {
+      messageApi.open({
+        type: 'error',
+        content: msg,
+        duration: 5,
+      });
+    }
+    const success = (msg: string) => {
+      messageApi.open({
+        type: 'success',
+        content: msg,
+        duration: 2,
+      });
+    }
 
   useEffect(() => {
-    setSelectedValue(range.start);
-  }, [range]);
+    setSelectedValue(dateRange.start);
+  }, [dateRange]);
+
+  // 初回ローカルデータセット
+  useEffect(() => {
+    setShowLoading(true);
+    let tmpDateRange: DateRange;
+    // イベント全般のデータを取得
+    axios.get(`${process.env.REACT_APP_API_URL}/events/${eventId}/${hash}`)
+    .then((res) => {
+      setEventName(res.data['name']);
+      tmpDateRange = {
+        start: dayjs(res.data['start_date']),
+        end: dayjs(res.data['end_date']),
+      };
+      setDateRange(tmpDateRange);
+      return axios.post(`${process.env.REACT_APP_API_URL}/schedules/${userId}/${hash}`, { password: password });
+    })
+    // ユーザーのスケジュールを取得
+    .then((res) => {
+      setData(initData(tmpDateRange,
+        res.data.map((d: any) => {
+          return {
+            date: dayjs(d['date']),
+            status: d['status']
+          } as Schedule;
+        })));
+    })
+    .catch((err) => {
+      error('データの取得に失敗しました');
+    })
+    .finally(() => {
+      setShowLoading(false);
+    });
+  }, []);
+
+  // rangeに対応するローカルデータを初期化
+  const initData = (range: DateRange, data: Schedule[]) => {
+    const numDays = range.end.diff(range.start, 'day') + 1;
+    return Array.from({ length: numDays }, (_, i) => {
+      const status = data.find((d) => d.date.isSame(range.start.add(i, 'day')))?.status || 'available';
+      return {
+        id: i,
+        date: range.start.add(i, 'day'),
+        status: status
+      } as Schedule;
+    });
+  }
+
+  // idに対応するローカルデータを更新
+  const setStatusByDate = (date: Dayjs) => {
+    setData((prev) => {
+      return prev.map((d) => {
+        if (d.date.isSame(date)) {
+          return { ...d, status: selectedStatus };
+        }
+        return d;
+      });
+    });
+  };
+
+  // データを送信
+  const saveData = () => {
+    setShowLoading(true);
+    const formatData = data.map((d) => {
+      return {
+        ...d,
+        date: d.date.format('YYYY-MM-DD'),
+      };
+    });
+    axios.post(`${process.env.REACT_APP_API_URL}/schedules/${userId}/${hash}/update`, { password: password, scheduleArray: formatData })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        error('データの保存に失敗しました');
+      })
+      .finally(() => {
+        setShowLoading(false);
+        success('データを保存しました');
+      });
+  }
 
   const dateCellRender = (value: Dayjs) => {
     const status = data.find((d) => d.date.isSame(value))?.status || "";
@@ -41,10 +149,12 @@ export const RegisterCalendar = (props: { data: Schedule[], range: DateRange, se
 
   return (
     <>
+      {contextHolder}
+      <StatusRadio selectedStatus={selectedStatus} setSelectedStatus={setSelectedStatus} />
       <ConfigProvider locale={jaJP}>
         <Calendar
           cellRender={dateCellRender}
-          validRange={[range.start, range.end]}
+          validRange={[dateRange.start, dateRange.end]}
           value={selectedValue}
           onSelect={(date: Dayjs) => {
             setStatusByDate(date);
@@ -52,6 +162,8 @@ export const RegisterCalendar = (props: { data: Schedule[], range: DateRange, se
           }}
         />
       </ConfigProvider>
+      <Button variant="contained" color="primary" onClick={saveData}>保存</Button>
+      <LoadingBackdrop isShow={showLoading} />
     </>
   );
 };
--- fe/src/pages/Register.tsx
+++ fe/src/pages/Register.tsx
@@ -1,128 +1,11 @@
-import { Button, Container } from "@mui/material";
-import { LoadingBackdrop, RegisterCalendar, StatusRadio, PasswordBox } from "../components";
-import { Dayjs } from "dayjs";
-import dayjs from 'dayjs';
-import { DateRange } from "../models";
-import { useEffect, useState } from "react";
-import { Schedule } from "../models";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import { message } from "antd";
-import { set } from "react-hook-form";
-
-// 仮データ
-
-const userId = "1";
+import { Container } from "@mui/material";
+import { RegisterCalendar, PasswordBox } from "../components";
+import { useState } from "react";
 
 export const Register = () => {
-  // パスパラメータ取得
-  const { eventId, hash } = useParams<{eventId: string, hash: string}>();
-
-  const [messageApi, contextHolder] = message.useMessage();
-  const [eventName, setEventName] = useState<string>('');
-  const [data, setData] = useState<Schedule[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<string>('unavailable');
-  const [dateRange, setDateRange] = useState<DateRange>({start: dayjs(), end: dayjs()});
-  const [showLoading, setShowLoading] = useState<boolean>(false);
   const [showContent, setShowContent] = useState<boolean>(false);
   const [password, setPassword] = useState<string>('');
-
-  // メッセージ表示
-  const error = (msg: string) => {
-    messageApi.open({
-      type: 'error',
-      content: msg,
-      duration: 5,
-    });
-  }
-  const success = (msg: string) => {
-    messageApi.open({
-      type: 'success',
-      content: msg,
-      duration: 2,
-    });
-  }
-
-  // 初回ローカルデータセット
-  useEffect(() => {
-    if (!showContent) return;
-    setShowLoading(true);
-    let tmpDateRange: DateRange;
-    // イベント全般のデータを取得
-    axios.get(`${process.env.REACT_APP_API_URL}/events/${eventId}/${hash}`)
-    .then((res) => {
-      setEventName(res.data['name']);
-      tmpDateRange = {
-        start: dayjs(res.data['start_date']),
-        end: dayjs(res.data['end_date']),
-      };
-      setDateRange(tmpDateRange);
-      return axios.post(`${process.env.REACT_APP_API_URL}/schedules/${userId}/${hash}`, { password: password });
-    })
-    // ユーザーのスケジュールを取得
-    .then((res) => {
-      setData(initData(tmpDateRange,
-        res.data.map((d: any) => {
-          return {
-            date: dayjs(d['date']),
-            status: d['status']
-          } as Schedule;
-        })));
-    })
-    .catch((err) => {
-      error('データの取得に失敗しました');
-    })
-    .finally(() => {
-      setShowLoading(false);
-    });
-  }, [password]);
-
-  // rangeに対応するローカルデータを初期化
-  const initData = (range: DateRange, data: Schedule[]) => {
-    const numDays = range.end.diff(range.start, 'day') + 1;
-    return Array.from({ length: numDays }, (_, i) => {
-      const status = data.find((d) => d.date.isSame(range.start.add(i, 'day')))?.status || 'available';
-      return {
-        id: i,
-        date: range.start.add(i, 'day'),
-        status: status
-      } as Schedule;
-    });
-  }
-
-  // idに対応するローカルデータを更新
-  const setStatusByDate = (date: Dayjs) => {
-    setData((prev) => {
-      return prev.map((d) => {
-        if (d.date.isSame(date)) {
-          return { ...d, status: selectedStatus };
-        }
-        return d;
-      });
-    });
-  };
-
-  // データを送信
-  const saveData = () => {
-    setShowLoading(true);
-    const formatData = data.map((d) => {
-      return {
-        ...d,
-        date: d.date.format('YYYY-MM-DD'),
-      };
-    });
-    axios.post(`${process.env.REACT_APP_API_URL}/schedules/${userId}/${hash}/update`, { password: password, scheduleArray: formatData })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        error('データの保存に失敗しました');
-      })
-      .finally(() => {
-        setShowLoading(false);
-        success('データを保存しました');
-      });
-  }
+  const [userId, setUserId] = useState<string>('1');
 
   // パスワード認証
   const handleAuthenticate = (password: string) => {
@@ -134,19 +17,7 @@ export const Register = () => {
     <>
       <Container maxWidth='md'>
         {!showContent && <PasswordBox userId={userId} handleAuthenticate={handleAuthenticate} />}
-        {showContent &&
-        <>
-        {contextHolder}
-          <StatusRadio selectedStatus={selectedStatus} setSelectedStatus={setSelectedStatus} />
-          <RegisterCalendar
-            data={data}
-            range={dateRange}
-            setStatusByDate={setStatusByDate}
-          />
-          <Button variant="contained" color="primary" onClick={saveData}>保存</Button>
-          <LoadingBackdrop isShow={showLoading} />
-        </>
-        }
+        {showContent && <RegisterCalendar password={password} /> }
       </Container>
     </>
   );
