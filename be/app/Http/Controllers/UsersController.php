c854fd202097b8c3ffc869f0bfd5ad0067f64865
kuu13580 <13580kuu@gmail.com>
2023-05-28 03:38:11 +0900

    UserのgetAPIを実装

--- be/app/Http/Controllers/UsersController.php
+++ be/app/Http/Controllers/UsersController.php
@@ -12,13 +12,8 @@ class UsersController extends Controller
         $event = Event::find($event_id);
         // ハッシュ値チェック
         if ($hash != $event->hash) return $this->SendError('Hash is invalid.');
-        $users = User::where('event_id', $event_id)->get();
-        return response()->json(
-            $users,
-            200,
-            [],
-            JSON_UNESCAPED_UNICODE
-        );
+        $users = User::select('id', 'name')->where('event_id', $event_id)->get();
+        return $this->successData($users);
     }
 
     public function authenticate($user_id, $hash, Request $request){
