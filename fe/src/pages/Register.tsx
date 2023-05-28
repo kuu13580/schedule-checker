d6b13fca1ecdb80dd47eb1e0ce81789c0636e79c
kuu13580 <13580kuu@gmail.com>
2023-05-29 03:49:16 +0900

    change showContent

--- fe/src/pages/Register.tsx
+++ fe/src/pages/Register.tsx
@@ -3,13 +3,13 @@ import { RegisterCalendar, PasswordBox, UserSelector } from "../components";
 import { useState } from "react";
 
 export const Register = () => {
-  const [showContent, setShowContent] = useState<boolean>(false);
+  const [showContent, setShowContent] = useState<string>('PasswordBox');
   const [password, setPassword] = useState<string>('');
   const [userId, setUserId] = useState<string>('1');
 
   // パスワード認証
   const handleAuthenticate = (password: string) => {
-    setShowContent(true);
+    setShowContent("RegisterCalendar");
     setPassword(password);
   }
 
@@ -17,10 +17,10 @@ export const Register = () => {
     <>
       <Container maxWidth='md' sx={{my: 2}}>
         <UserSelector
-          resetView={() => {setPassword(''); setShowContent(false)}}
+          resetView={() => {setPassword(''); setShowContent("PasswordBox")}}
           setUserId={setUserId}/>
-        {!showContent && <PasswordBox userId={userId} handleAuthenticate={handleAuthenticate} />}
-        {showContent && <RegisterCalendar userId={userId} password={password} /> }
+        {showContent == "PasswordBox" && <PasswordBox userId={userId} handleAuthenticate={handleAuthenticate} />}
+        {showContent == "RegisterCalendar" && <RegisterCalendar userId={userId} password={password} /> }
       </Container>
     </>
   );
