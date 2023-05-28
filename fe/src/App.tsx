ff0a00c4d077345c0baf7c34d6833a4e538fa480
kuu13580 <13580kuu@gmail.com>
2023-05-29 02:12:49 +0900

    minor fix

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,10 @@
 // import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Create, Home } from './pages';
+import { Create, Home, Register } from './pages';
 import dayjs from 'dayjs';
 import weekday from 'dayjs/plugin/weekday';
 import 'dayjs/locale/ja';
-import { Register } from './pages';
 
 dayjs.extend(weekday);
 dayjs.locale('ja');
@@ -16,8 +15,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/register/:eventId/:hash" element={<Register/>} />
         <Route path="/create" element={<Create/>} />
+        <Route path="/register/:eventId/:hash" element={<Register/>} />
       </Routes>
     </Router>
   );
