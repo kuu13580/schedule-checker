fc1fdd4feb7a94f7d9fc7720619e99132031c09b
kuu13580 <13580kuu@gmail.com>
2023-05-29 23:02:57 +0900

    fix layout

--- fe/src/pages/Create.tsx
+++ fe/src/pages/Create.tsx
@@ -1,12 +1,11 @@
 import { Controller, useForm } from "react-hook-form";
-import { Button, Container, TextField } from "@mui/material";
+import { Button, Container, TextField, Grid } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import ja from 'dayjs/locale/ja';
 import { EventForm } from "../models";
 import { useEffect, useState } from "react";
-import { Box } from "@mui/system";
 import axios from "axios";
 
 
@@ -59,69 +58,82 @@ export const Create = () => {
 
   return (
     <Container maxWidth={'lg'}>
-      <h1>Create未実装</h1>
-      <Box sx={{ display: 'flex', flexDirection: "column", width: {xs: '90svw', sm: '550px'}, mx: 'auto' }}>
-        <TextField
-          required
-          label="イベント名(最大20文字)"
-          variant="outlined"
-          inputProps={{ maxLength: 20 }}
-          {...register('name', { required: true, maxLength: 20 })}
-          error={"name" in errors} />
-        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={ja}>
-          <Controller
-            name="startDate"
-            control={control}
-            render={({ field }) => (
-              <DatePicker
-                {...field}
-                label="始点日"
-                format="YYYY/M/D"
-              />
-            )} />
-          <Controller
-            name="endDate"
-            control={control}
-            render={({ field }) => (
-              <DatePicker
-                {...field}
-                label="終点日"
-                format="YYYY/M/D"
-                slotProps={{
-                  textField: {
-                    helperText: dateError && '日付が前後しています',
-                  },
-                }}
-              />
-            )} />
-        </LocalizationProvider>
-        <TextField
-          required
-          label="主催者名（最大10文字）"
-          inputProps={
-            { maxLength: 10 }
-          }
-          variant="outlined"
-          {...register('ownerName', { required: true, maxLength: 10 })}
-          error={"ownerName" in errors} />
-        <TextField
-          required
-          label="パスワード(数字4桁)"
-          type="password"
-          variant="outlined"
-          inputProps={{ maxLength: 4 }}
-          {...register('password', { required: true, pattern: /^[0-9]{4}$/ })}
-          error={"password" in errors} />
-        <Button
-          variant="contained"
-          onClick={handleSubmit(onSubmit)}>
-          作成
-        </Button>
-        <Button
-          onClick={() => console.log(errors)}>
-          デバッグ
-        </Button>
-      </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="イベント名(最大20文字)"
+              variant="outlined"
+              inputProps={{ maxLength: 20 }}
+              {...register('name', { required: true, maxLength: 20 })}
+              error={"name" in errors} />
+          </Grid>
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={ja}>
+            <Grid item xs={12}>
+              <Controller
+                name="startDate"
+                control={control}
+                render={({ field }) => (
+                  <DatePicker
+                    {...field}
+                    label="始点日"
+                    format="YYYY/M/D"
+                  />
+                )} />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                name="endDate"
+                control={control}
+                render={({ field }) => (
+                  <DatePicker
+                    {...field}
+                    label="終点日"
+                    format="YYYY/M/D"
+                    slotProps={{
+                      textField: {
+                        helperText: dateError && '日付が前後しています',
+                      },
+                    }}
+                  />
+                )} />
+            </Grid>
+          </LocalizationProvider>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="主催者名（最大10文字）"
+              inputProps={
+                { maxLength: 10 }
+              }
+              variant="outlined"
+              {...register('ownerName', { required: true, maxLength: 10 })}
+              error={"ownerName" in errors} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="パスワード(数字4桁)"
+              type="password"
+              variant="outlined"
+              inputProps={{ maxLength: 4 }}
+              {...register('password', { required: true, pattern: /^[0-9]{4}$/ })}
+              error={"password" in errors} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              onClick={handleSubmit(onSubmit)}>
+              作成
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              onClick={() => console.log(errors)}>
+              デバッグ
+            </Button>
+          </Grid>
+        </Grid>
     </Container>
   )
 }
