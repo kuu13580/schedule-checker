f6737b57d498ec9b2763008366fbdb0bb01aaf08
kuu13580 <13580kuu@gmail.com>
2023-06-02 01:15:33 +0900

    feにユーザー削除APIを実装

--- fe/src/components/DeleteUserButton.tsx
+++ fe/src/components/DeleteUserButton.tsx
@@ -1,11 +1,21 @@
 import { Button } from '@mui/material';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export const DeleteUserButton = (props: {userId: string, password: string}) => {
+  const { hash } = useParams<{hash: string}>();
   const userId = props.userId;
   const password = props.password;
 
   const deleteUser = () => {
-    // TODO: ユーザー削除処理
+    axios.post(`${process.env.REACT_APP_API_URL}/users/${userId}/${hash}/delete`, {password: password})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      }
+    );
   }
 
   return (
