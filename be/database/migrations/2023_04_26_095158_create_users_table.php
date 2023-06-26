7918d933759fe2842fe70a5ec33881463c70741c
kuu13580 <13580kuu@gmail.com>
2023-06-27 01:34:11 +0900

    fix メッセージのnullable

--- be/database/migrations/2023_04_26_095158_create_users_table.php
+++ be/database/migrations/2023_04_26_095158_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->bigInteger('event_id')->unsigned()->nullable(false);
             $table->string('name')->nullable(false);
             $table->string('password')->nullable(false);
-            $table->string('message')->nullable(false)->default('');
+            $table->string('message')->nullable(true)->default('');
             $table->timestamps();
         });
     }
